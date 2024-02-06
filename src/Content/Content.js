98db5c8834d071f7363ac3efafd1483883d693db
Ajithkanna21 <ajithkanna.thiyagu@crud-operations.com>
2024-02-06 18:33:54 +0530

    changes in popup

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -2,31 +2,31 @@ import React, { useContext, useState, useEffect } from 'react';
 import './Content.css';
 import { AppContext } from '../App';
 import { PlusCircleTwoTone, CheckOutlined } from '@ant-design/icons';
-import { Button, Popconfirm, Input, List, } from 'antd';
+import { Button, Popconfirm, Input, List, message} from 'antd';
 import axios from 'axios';
 
 function Content() {
     const { username, password, fetchData, data } = useContext(AppContext); //useContext
     const bla = data.find(v => v.username === username && v.password === password); // Logined user data
-    const [ inputValue, setInputValue ] = useState(''); 
+    const [ inputValue, setInputValue ] = useState('');
     const [ list, setList ] = useState([]);
     const [ cl, setCl ] = useState([]);
     const [ h, setH ] = useState([]);
     const currentDate = new Date();
     const formattedDate = `${currentDate.getDate()}|${currentDate.getMonth() + 1}|${currentDate.getFullYear()}`;  //current date
-    const previousDate = `${currentDate.getDate()-1}|${currentDate.getMonth() + 1}|${currentDate.getFullYear()}`;  //previous date
+    const previousDate = `${currentDate.getDate() - 1}|${currentDate.getMonth() + 1}|${currentDate.getFullYear()}`;  //previous date
 
-    useEffect( () => {
+    useEffect(() => {
         // Fetch the complete data associated with the user upon login
-         axios.get(`http://localhost:3000/data/${bla.id}`)
+        axios.get(`http://localhost:3000/data/${bla.id}`)
             .then(response => {
                 const { complete } = response.data; // Assuming the complete data is stored in 'complete' property
-                const d = complete.map(i=>i)
-                const c = d.filter(i=>i.date === formattedDate)
+                const d = complete.map(i => i)
+                const c = d.filter(i => i.date === formattedDate)
                 setCl(c || []); // Update the completed list state with the fetched data
                 const { todo } = response.data; // Assuming the complete data is stored in 'complete' property
                 setList(todo || []); // Update the todo list state with the fetched data
-                const f = d.filter(i=>i.date === previousDate)
+                const f = d.filter(i => i.date === previousDate)
                 setH(f || []); // Update the todo list state with the fetched data
             })
             .catch(error => {
@@ -43,6 +43,7 @@ function Content() {
         axios.put(`http://localhost:3000/data/${bla.id}`, { ...bla, todo: [ ...list, newTodo ] }).then(() => fetchData());
         setList([ ...list, newTodo ]);
         setInputValue('');
+        message.success('successfully added');
     };
     // --------------------------------------------Check btn-------------------------------------------
     const checkBtn = (index) => {
@@ -63,8 +64,8 @@ function Content() {
                     placement="topLeft"
                     title={<Input placeholder="Enter something" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />}
                     description=''
-                    okText='ok'
-                    cancelText="cancel"
+                    okText='yes'
+                    cancelText="no"
                     onConfirm={handleAddClick}
                 >
                     <Button type="primary" className='btn' shape="" icon={<PlusCircleTwoTone />} size="large">Add</Button>
@@ -105,7 +106,7 @@ function Content() {
                 <div className='d'>
                     <h3>Complete</h3>
                     <List
-                    className='c'
+                        className='c'
                         size="small"
                         dataSource={cl}
                         renderItem={(item, i) => (
